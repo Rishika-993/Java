df6c133842ddbdac61fcd554e24fb65343295936
Rishika-993 <rishikaagrawal22@gmail.com>
2024-05-23 23:46:00 +0530

    classes

--- OOPS/classes/Main.java
+++ OOPS/classes/Main.java
@@ -15,20 +15,24 @@ public class Main {
         float[] marks = new float[5];
 
         Student students = new Student();
+        // System.out.println(Arrays.toString(students);     --returns every value to be null
 
-        Student rishika;
+        Student rishika;  // ---declaration---
         // A class is a named group of properties and methods
         // A class is a template of an object and an object is an instance of a class
         // A class is a logical construct and  an object is a physical reality and occupies space in memory
         // A class is a user defined data type and an object is a variable of that data type
         // A class is a blueprint for an object
+
+        rishika = new Student();  // ---instantiation---   dynamically allocates memory
     }
 }
 
 //create a class named as Student
+// for every single student 
 
 class Student{
-    int[] rollno = new int[5];
-    String[] name = new String[5];
-    float[] marks = new float[5];
+    int rollno;
+    String name;
+    float marks;
 }
