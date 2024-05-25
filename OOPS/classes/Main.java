26b9d843361cfcdf69a0017057ac4ae8becf6fe2
Rishika-993 <rishikaagrawal22@gmail.com>
2024-05-26 00:11:27 +0530

    constructor overloading

--- OOPS/classes/Main.java
+++ OOPS/classes/Main.java
@@ -20,7 +20,7 @@ public class Main {
         Student rishika;  // ---declaration---                 statically allocates memory at compile time 
         // A class is a named group of properties and methods
         // A class is a template of an object and an object is an instance of a class
-        // A class is a logical construct and  an object is a physical reality and occupies space in memory
+        // A class is a logical construct and  an object is a physical reality and occupies     space in memory
         // A class is a user defined data type and an object is a variable of that data type
         // A class is a blueprint for an object
 
@@ -33,6 +33,7 @@ public class Main {
         // A constructor is a speacial function that runs when you create an object and it allocates some variables
 
         Student yashi = new Student(13, "Yashi", 90.0f);
+        yashi.greeting
     }
 }
 
@@ -54,5 +55,23 @@ class Student{
         this.name = "rishika agarwal";
         this.marks = 88f;
     }   
+
+    void greeting(){
+        System.out.println("Hello, I am a student, My name is" + name);
+    }   
+
+    // Student arpit = new Student(13, "Arpit", 90.0f);
+    // here this will be replaced with arpit
+    Student(int rollno, String name, float marks){
+        this.rollno = rollno;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    Student ( Student student){
+        this.rollno = student.rollno;
+        this.name = student.name;
+        this.marks = student.marks;
+    }
 
 }
