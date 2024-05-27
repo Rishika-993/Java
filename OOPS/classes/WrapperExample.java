c0c85ddd93d06a440981fb51d5823692d82fcb34
Rishika-993 <rishikaagrawal22@gmail.com>
2024-05-27 23:44:11 +0530

    final keyword

--- OOPS/classes/WrapperExample.java
+++ OOPS/classes/WrapperExample.java
@@ -10,6 +10,15 @@ public class WrapperExample {
         swap(a, b);
         System.out.println(a + " " + b);  //10 20, do not sap because the values are passed by value and not reference
 
+        // final keyword does not allow the value to be changed
+        // final int bonus = 2;
+        // bonus = 3; // error
+
+        final A rishika = new A("rishika agarwal");
+        rishika.name = "Other name";
+
+        //when a non-primitive is final, you can't reassing it
+        rishika = new A("new name");  // error
     }
 
     static void swap(int a, int b){
@@ -19,5 +28,18 @@ public class WrapperExample {
     }
 
     // but when we pass object, reference value is passed
-    // therefore, the values are swapped
+    // then also on changing int with integer, value is not swapped as in integer definition final keyword is used  
+}
+
+
+// final variables has to be initialized
+// always initialize it while declaring it
+
+class A {
+    final int a = 10;
+    String name;
+
+    public A(String name){
+        this.name = name;
+    }
 }
